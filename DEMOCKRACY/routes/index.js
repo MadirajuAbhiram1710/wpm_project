b8263ba767fa01a7dfb81cb86204fe9130f7e2d1
Abhiram <23eg107b34@anurag.edu.in>
2025-08-20 11:16:29 +0530

    Initial commit

--- DEMOCKRACY/routes/index.js
+++ DEMOCKRACY/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'democracy' });
+  res.render('index', { title: 'Democracy' });
 });
 
 module.exports = router;
